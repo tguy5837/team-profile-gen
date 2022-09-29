26a88b8a652cfba7bb4cb936bf648770cddeff0b
DTGuy <tguy5837@gmail.com>
2022-09-29 18:46:27 -0400

    completed and tested Engineer object

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -4,11 +4,28 @@ jest.mock('../lib/Employee');
 
 const Engineer = require('../lib/Engineer');
 
-test('make sure new employee passes properties and methods to engineer', () => {
-    const engineer = new Engineer('github');
+test('make sure engineer gets employee properties', () => {
+    const engineer = new Engineer('John', '21', 'johndoe@example.com');
 
     expect(engineer.name).toBe('John');
     expect(engineer.id).toBe('21');
     expect(engineer.email).toBe('johndoe@example.com');
 });
 
+test('make sure github property is added', () => {
+    const engineer = new Engineer('John', '21', 'johndoe@example.com', 'github');
+
+    expect(engineer.github).toBe('github');
+});
+
+test('should return github from getGithub()', () => {
+    const engineer = new Engineer('John', '21', 'johndoe@example.com', 'github');
+
+    expect(engineer.getGithub()).toBe('github');
+});
+
+test('should return Engineer from getRole()', () => {
+    const engineer = new Engineer('John', '21', 'johndoe@example.com', 'github');
+
+    expect(engineer.getRole()).toBe('Engineer');
+})
