45a876d3594fdd99a398a0870cf3c8226967e6b7
DTGuy <tguy5837@gmail.com>
2022-10-01 17:52:05 -0400

    added managers name to header

--- src/html-template.js
+++ src/html-template.js
@@ -111,7 +111,7 @@ module.exports = teamArr => {
         <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
         <path fill-rule="evenodd" d="M5.216 14A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216z"/>
         <path d="M4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5z"/>
-      </svg> My Team</h1>
+      </svg> ${manager.name}'s Team</h1>
     </header>
     <main>
         <div class="container d-flex flex-wrap justify-content-center p-5">
